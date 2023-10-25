80b2fe1a49cac6445da7dae4e1e574572f5c2e57
Were Samson Bruno <werefast2000@gmail.com>
2023-10-25 22:48:44 +0000

    Import Keys from env file into hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
+import 'dotenv/config';
+require('dotenv').config();
+
 require("@nomicfoundation/hardhat-toolbox");
+
+// Go to https://alchemy.com, sign up, create a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY; // Remove the quotes
+
+// Replace this private key with your Sepolia account private key
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY; // Remove the quotes
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`, // Use the variable directly
+      accounts: [SEPOLIA_PRIVATE_KEY] // Use the variable directly
+    }
+  }
 };
