05f0de8132005fa9c0778a6da0d6c6996df89ed6
Were Samson Bruno <werefast2000@gmail.com>
2023-10-26 10:42:21 +0000

    change deploy statement

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const minimumCreditScore = 720;
+  const loanAmount = hre.ethers.parseEther("0.002");
+  const creditScore = 890;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const LoanContract = await hre.ethers.getContractFactory("LoanContract");
+  const loanContract = await LoanContract.deploy(minimumCreditScore);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log(`LoanContract deployed with loan amount ${loanAmount}ETH and credit score ${creditScore}`);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // The contract is NOT deployed yet; we must wait until it is mined
+  const result = await loanContract.minimumCreditScore();
+  console.log("Result of Analysis:", result);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
